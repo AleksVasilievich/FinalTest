e701b48fbcb7be1a491adddae33e00223b074819
AleksVasilievich <104069090+AleksVasilievich@users.noreply.com>
2023-09-29 04:23:53 +0300

    Add command

--- src/AnimalsProgram/Main.java
+++ src/AnimalsProgram/Main.java
@@ -1,16 +1,17 @@
 package AnimalsProgram;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
         try {
             Dog dog1 = new Dog("Дружок", "2020-09-09");
+            dog1.addCommand("Сидеть");
             Cat cat1 = new Cat("Мурзик", "2019-08-08");
+            cat1.addCommand("Прижок");
             Donkey donkey1 = new Donkey("Лунтик", "2018-07-07");
+            donkey1.addCommand("Стоять");
 
             List<Animal> animals = new ArrayList<>();
             animals.add(dog1);
@@ -19,14 +20,29 @@ public class Main {
             AnimalCounter.incrementCount();
             animals.add(donkey1);
             AnimalCounter.incrementCount();
+
+
+
             for (Animal animal : animals) {
                 System.out.println(animal.getName() + ": " + animal.getBirthDate());
             }
             System.out.println("Общее количество животных: " + AnimalCounter.getCount());
+
+            System.out.println("Список животных:");
+            for (Animal animal : animals) {
+                System.out.println("- " + animal.getName());
+            }
+            System.out.println();
+            for (Animal animal : animals) {
+                System.out.println("Животное: " + animal.getName());
+                System.out.println("Команды:");
+                for (String command : animal.getCommands()) {
+                    System.out.println("- " + command);
+                }
+                System.out.println();
+            }
+
         } catch (Exception e) {e.printStackTrace();}
-
-
-
 
     }
 }
