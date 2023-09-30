5525c298b9540fff324262fb6f24286d6b2b89ea
AleksVasilievich <104069090+AleksVasilievich@users.noreply.com>
2023-09-30 18:13:11 +0300

    add menu

--- src/AnimalsProgram/Main.java
+++ src/AnimalsProgram/Main.java
@@ -5,50 +5,8 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-//    public static void main(String[] args) {
-//
-//       try {
-//
-//            Dog dog1 = new Dog("Дружок", "2020-09-09");
-//            dog1.addCommand("Сидеть");
-//            Cat cat1 = new Cat("Мурзик", "2019-08-08");
-//            cat1.addCommand("Прижок");
-//            Donkey donkey1 = new Donkey("Лунтик", "2018-07-07");
-//            donkey1.addCommand("Стоять");
-//
-//            List<Animal> animals = new ArrayList<>();
-//            animals.add(dog1);
-//            AnimalCounter.incrementCount();
-//            animals.add(cat1);
-//            AnimalCounter.incrementCount();
-//            animals.add(donkey1);
-//            AnimalCounter.incrementCount();
-//
-//
-//
-//            for (Animal animal : animals) {
-//                System.out.println(animal.getName() + ": " + animal.getBirthDate());
-//            }
-//            System.out.println("Общее количество животных: " + AnimalCounter.getCount());
-//
-//            System.out.println("Список животных:");
-//            for (Animal animal : animals) {
-//                System.out.println("- " + animal.getName());
-//            }
-//            System.out.println();
-//            for (Animal animal : animals) {
-//                System.out.println("Животное: " + animal.getName());
-//                System.out.println("Команды:");
-//                for (String command : animal.getCommands()) {
-//                    System.out.println("- " + command);
-//                }
-//                System.out.println();
-//            }
-//
-//        } catch (Exception e) {e.printStackTrace();}
-//
-//    }
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
         int choice;
         boolean exit = false;
